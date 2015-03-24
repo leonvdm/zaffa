95fc85e39c6b4bb0348b220cb8065a88a4d71e4a
Leon van der Merwe <leonvdm2@gmail.com>
2015-03-24 18:08:33 +0200

    Added logging

--- src/main/java/com/zaffa/industrial/controller/LoginController.java
+++ src/main/java/com/zaffa/industrial/controller/LoginController.java
@@ -1,16 +1,19 @@
 package com.zaffa.industrial.controller;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 	
-	static Logger log = Logger.getLogger(LoginController.class.getName());
+	static Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
 	
 	@RequestMapping("/login")
 	public String login() {
+		LOGGER.info("Logging in");
 		return "login";
 	}
 }
