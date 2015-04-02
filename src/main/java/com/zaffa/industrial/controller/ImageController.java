e02777f2f893be48629d76c339f9f38e5da55890
Leon van der Merwe <leonvdm2@gmail.com>
2015-04-02 14:51:10 +0200

    Added ImageController

---
+++ src/main/java/com/zaffa/industrial/controller/ImageController.java
@@ -0,0 +1,19 @@
+package com.zaffa.industrial.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ImageController {
+	
+	static Logger logger = LoggerFactory.getLogger(ImageController.class);
+	
+	@RequestMapping("/upload") 
+	public void upload() {
+		
+	}
+	
+
+}
